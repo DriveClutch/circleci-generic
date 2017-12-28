d5add71144b4b8f34c27478d99cef36e7f728a96
David Hallum <david@driveclutch.com>
2017-12-28 13:27:21 -0500

    Add GCP cred handling to add-helm-repos.sh [#153908537]

--- tools/add-helm-repos.sh
+++ tools/add-helm-repos.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash -e
+
+echo $GCP_AUTH_KEY | base64 -d - > ${HOME}/gcp-key.json
+export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcp-key.json
 
 for i in $(gsutil ls ${HELM_GS_BUCKET} | awk -F'/' '{ print $4 }')
 do
