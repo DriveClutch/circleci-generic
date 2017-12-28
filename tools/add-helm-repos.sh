ce6e0ed27243a4a78b1e1ec2e3bb22a149f83b4e
David Hallum <david@driveclutch.com>
2017-12-28 13:51:27 -0500

    Add debuglog capabilities for add-helm-repos.sh [#153908537]

--- tools/add-helm-repos.sh
+++ tools/add-helm-repos.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+if [[ -f ".circleci/debuglog" ]]; then
+	set -x
+fi
+
 echo $GCP_AUTH_KEY | base64 -d - > ${HOME}/gcp-key.json
 export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcp-key.json
 
