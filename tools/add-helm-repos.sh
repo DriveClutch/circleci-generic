dab6b49e237201280849af3e8e5a9f8e626adbcf
David Hallum <david@driveclutch.com>
2017-12-28 14:20:26 -0500

    Still working with gsutil auth options [#153908537]

--- tools/add-helm-repos.sh
+++ tools/add-helm-repos.sh
@@ -7,6 +7,8 @@ fi
 echo $GCP_AUTH_KEY | base64 -d - > ${HOME}/gcp-key.json
 export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcp-key.json
 
+gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
+
 for i in $(gsutil ls ${HELM_GS_BUCKET} | awk -F'/' '{ print $4 }')
 do
 	helm repo add ${i} ${HELM_GS_BUCKET}$i
